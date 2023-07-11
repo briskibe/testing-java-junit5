94f4a0ecf049a73bfa7ad244092bcb1005167f0d
briskibe <bernard.briski@analyticom.de>
2023-07-12 00:16:40 +0200

    Testing IndexController, added different types of assertEquals

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index(), "Wrong view returned");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
+                "Message to build" +
+                "for my test");
+    }
+}
