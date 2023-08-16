9769477adbec0f1b5ec2180ee014725869d9356a
briskibe <bernard.briski@analyticom.de>
2023-08-16 18:19:38 +0200

    Repeated test example

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +27,12 @@ class PersonTest implements ModelTests {
         assertAll("Test Props Set",
                 () -> assertEquals("Joe", person.getFirstName(), () -> "First Name Failed"),
                 () -> assertEquals("Buck", person.getLastName(), () -> "Last Name Failed"));
+    }
+
+    @Test
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    void myRepeatedTest() {
+        // todo - impl
     }
 }
